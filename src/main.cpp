25886871f1be7d1c8540e785ef72cae8d647cbe3
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2023-07-24 18:45:27 +0200

    Add image loading from sdcard, select to delete, handle no images

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <SDL2/SDL_image.h>
 #include <SDL_FontCache.h>
 #include <coreinit/memory.h>
+#include <filesystem>
 #include <iostream>
 #include <romfs-wiiu.h>
 #include <string>
@@ -18,14 +19,53 @@
 #define FONT_SIZE          28
 #define SCREEN_COLOR_WHITE ((SDL_Color){.r = 0xFF, .g = 0xFF, .b = 0xFF, .a = 0xFF})
 
-const std::string imagePath = "romfs:/";
+const std::string imagePath = "fs:/vol/external01/wiiu/screenshots/";
 FC_Font *font;
 
 struct Image {
     SDL_Texture *texture;
     int x, y;
     bool selected;
+    std::string path;
 };
+
+std::vector<Image> scanImagesInSubfolders(const std::string &directoryPath, SDL_Renderer *renderer, int offsetX, int offsetY) {
+    std::vector<Image> images;
+    int imageIndex = 1;
+
+    if (!std::filesystem::exists(directoryPath) || !std::filesystem::is_directory(directoryPath)) {
+        return images; // Return an empty vector if the directory doesn't exist or is not a directory
+    }
+
+    for (const auto &entry : std::filesystem::recursive_directory_iterator(directoryPath)) {
+        if (!entry.is_regular_file()) {
+            continue; // Skip if not a regular file
+        }
+
+        std::string extension = entry.path().extension().string();
+        if (extension == ".jpg" || extension == ".png" || extension == ".bmp") {
+            SDL_Surface *surface = IMG_Load(entry.path().string().c_str());
+            if (!surface) {
+                continue; // Failed to load the image, skip it
+            }
+
+            Image img;
+            img.texture = SDL_CreateTextureFromSurface(renderer, surface);
+            SDL_FreeSurface(surface);
+
+            img.x = offsetX + (imageIndex - 1) % GRID_SIZE * (IMAGE_SIZE + SEPARATION);
+            img.y = offsetY + (imageIndex - 1) / GRID_SIZE * (IMAGE_SIZE + SEPARATION);
+
+            img.selected = false;
+            img.path = entry.path().string();
+
+            images.push_back(img);
+            imageIndex++;
+        }
+    }
+
+    return images;
+}
 
 int main() {
     romfsInit();
@@ -47,38 +87,13 @@ int main() {
         return 1;
     }
 
-    std::vector<Image> images;
-
-    int imageIndex = 1;
-    bool foundImage = true;
-
     int totalWidth = GRID_SIZE * (IMAGE_SIZE + SEPARATION) - SEPARATION;
     int totalHeight = GRID_SIZE * (IMAGE_SIZE + SEPARATION) - SEPARATION + MARGIN_TOP + MARGIN_BOTTOM;
 
     int offsetX = (SCREEN_WIDTH - totalWidth) / 2 - IMAGE_SIZE / 3;
     int offsetY = (SCREEN_HEIGHT - totalHeight) / 2 + MARGIN_TOP;
 
-    while (foundImage) {
-        std::string filename = imagePath + "image" + std::to_string(imageIndex) + ".png";
-        SDL_Surface *surface = IMG_Load(filename.c_str());
-
-        if (!surface) {
-            foundImage = false;
-        } else {
-            Image img;
-            img.texture = SDL_CreateTextureFromSurface(renderer, surface);
-            SDL_FreeSurface(surface);
-
-            img.x = offsetX + (imageIndex - 1) % GRID_SIZE * (IMAGE_SIZE + SEPARATION);
-            img.y = offsetY + (imageIndex - 1) / GRID_SIZE * (IMAGE_SIZE + SEPARATION);
-
-            img.selected = false;
-
-            images.push_back(img);
-        }
-
-        imageIndex++;
-    }
+    std::vector<Image> images = scanImagesInSubfolders(imagePath, renderer, offsetX, offsetY);
 
     int selectedImageIndex = 0;
     int scrollOffsetY = 0;
@@ -115,9 +130,14 @@ int main() {
                     selectedImageIndex++;
                 }
             } else if (input.get(TRIGGER, PAD_BUTTON_X)) {
-                SDL_DestroyTexture(images[selectedImageIndex].texture);
-                images[selectedImageIndex].texture = nullptr;
-                images.erase(images.begin() + selectedImageIndex);
+                for (auto image : images) {
+                    if (image.selected) {
+                        SDL_DestroyTexture(images[selectedImageIndex].texture);
+                        images[selectedImageIndex].texture = nullptr;
+                        remove(images[selectedImageIndex].path.c_str());
+                        images.erase(images.begin() + selectedImageIndex);
+                    }
+                }
 
                 int totalImages = static_cast<int>(images.size());
                 for (int i = 0; i < totalImages; ++i) {
@@ -130,29 +150,30 @@ int main() {
         }
 
         SDL_RenderClear(renderer);
-
-        for (size_t i = 0; i < images.size(); ++i) {
-            SDL_Rect destRect = {images[i].x, images[i].y + scrollOffsetY, IMAGE_SIZE, IMAGE_SIZE};
-            if (images[i].selected) {
-                SDL_SetTextureColorMod(images[i].texture, 0, 255, 0);
-            } else {
-                SDL_SetTextureColorMod(images[i].texture, 255, 255, 255);
+        if (images.empty()) {
+            FC_Draw(font, renderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, "No images found");
+        } else {
+            for (size_t i = 0; i < images.size(); ++i) {
+                SDL_Rect destRect = {images[i].x, images[i].y + scrollOffsetY, IMAGE_SIZE, IMAGE_SIZE};
+                if (images[i].selected) {
+                    SDL_SetTextureColorMod(images[i].texture, 0, 255, 0);
+                } else {
+                    SDL_SetTextureColorMod(images[i].texture, 255, 255, 255);
+                }
+                SDL_SetTextureBlendMode(images[i].texture, SDL_BLENDMODE_BLEND);
+                SDL_RenderCopy(renderer, images[i].texture, nullptr, &destRect);
             }
-            SDL_SetTextureBlendMode(images[i].texture, SDL_BLENDMODE_BLEND);
-            SDL_RenderCopy(renderer, images[i].texture, nullptr, &destRect);
+
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            SDL_Rect outlineRect = {images[selectedImageIndex].x, images[selectedImageIndex].y + scrollOffsetY, IMAGE_SIZE + 5, IMAGE_SIZE + 5};
+            SDL_RenderDrawRect(renderer, &outlineRect);
         }
-
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_Rect outlineRect = {images[selectedImageIndex].x, images[selectedImageIndex].y + scrollOffsetY, IMAGE_SIZE + 5, IMAGE_SIZE + 5};
-        SDL_RenderDrawRect(renderer, &outlineRect);
-
         SDL_SetRenderDrawColor(renderer, 0, 0, 139, 255);
 
         SDL_Rect hudRect = {SCREEN_WIDTH - IMAGE_SIZE - SEPARATION, 0, IMAGE_SIZE, SCREEN_HEIGHT};
         SDL_RenderFillRect(renderer, &hudRect);
 
         SDL_SetRenderDrawColor(renderer, 173, 216, 230, 255);
-
         // Draw hud text
         FC_Draw(font, renderer, SCREEN_WIDTH - IMAGE_SIZE - SEPARATION + 5, 5, "Album");
         std::string allImagesCount = "(" + std::to_string(images.size()) + ")";
